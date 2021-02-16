608f89a45b225c623f9e376b7c13d76eee058041
mahmoud717 <mahmoudmohammad717@gmail.com>
2021-02-16 16:46:38 +0200

    added bicycle table to the db

--- db/migrate/20210216142657_create_bicycles.rb
+++ db/migrate/20210216142657_create_bicycles.rb
@@ -1,7 +1,9 @@
 class CreateBicycles < ActiveRecord::Migration[6.1]
   def change
     create_table :bicycles do |t|
-
+      t.string :name 
+      t.string :model
+      t.string :image_url
       t.timestamps
     end
   end
