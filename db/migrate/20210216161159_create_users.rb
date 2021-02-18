2800e245cf7e8690ca95c9af0f452591b2167d4f
mahmoud717 <mahmoudmohammad717@gmail.com>
2021-02-18 15:12:29 +0200

    finished modling the data

--- db/migrate/20210216161159_create_users.rb
+++ db/migrate/20210216161159_create_users.rb
@@ -3,7 +3,7 @@ class CreateUsers < ActiveRecord::Migration[6.1]
     create_table :users do |t|
       t.string :name
       t.string :email
-
+      t.boolean :admin , default: false
       t.timestamps
     end
   end
