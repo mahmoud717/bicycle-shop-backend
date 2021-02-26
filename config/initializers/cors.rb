cc401626a03a58a78f5ac39e7f0f1373d13e97ff
mahmoud717 <mahmoudmohammad717@gmail.com>
2021-02-27 01:54:51 +0200

    changes stuff

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,12 +7,9 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:3000"
+    origins "http://localhost:3000" ,"https://bicycle-shop-react.netlify.app"
     resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
   end
 
-  allow do
-    origins "https://jdh-authentication-app-react.herokuapp.com"
-    resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
-  end
+ 
 end
