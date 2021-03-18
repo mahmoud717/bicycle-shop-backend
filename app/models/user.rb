251caabd533d7acd2b1c9aac7cd4597cc6639ae0
mahmoud717 <mahmoudmohammad717@gmail.com>
2021-03-18 20:44:11 +0200

    changed favourites index

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,10 @@ class User < ApplicationRecord
   has_many :bicycles, through: :favourites, :dependent => :destroy
   has_many :orders, :dependent => :destroy
 
-  validates :email, presence: true, uniqueness: { case_sensitive: false }, format: { with: ConstantData::VALID_EMAIL_REGEX }
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
   validates :name, presence: true, length: { minimum: 3 }
+  validates_format_of :email,
+                      with: /\A\S+@.+\.\S+\z/
   validates :password, presence: :true, length: { minimum: 6 }
   
 end
