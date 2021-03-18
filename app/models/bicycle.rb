ad634d64f4ee25c59146213798d048798cc98224
mahmoud717 <mahmoudmohammad717@gmail.com>
2021-03-18 18:49:53 +0200

    fixed some minor issues

--- app/models/bicycle.rb
+++ app/models/bicycle.rb
@@ -3,4 +3,9 @@ class Bicycle < ApplicationRecord
   has_many :users, through: :favourites
   has_many :orders, :dependent => :destroy
   has_many :options, :dependent => :destroy
+
+  validates :name, presence: true
+  validates :model, presence: true
+  validates :image_url, presence: true
+  validates :description, presence: true
 end
--- app/models/option.rb
+++ app/models/option.rb
@@ -2,4 +2,6 @@ class Option < ApplicationRecord
     has_many :suboptions, :class_name => "Option", :foreign_key => 'parent_id', :dependent => :destroy
     belongs_to :parent_option, :class_name => "Option", optional: true
     belongs_to :bicycle
+    validates :name, presence: true
+    validates :value, presence: true
 end
